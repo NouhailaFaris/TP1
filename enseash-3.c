62a93ee015d0d495d6aebb03098b351c703b1f60
MohamedBenabdelhadi1 <152403377+MohamedBenabdelhadi1@users.noreply.github.com>
2023-12-01 18:10:21 +0100

    question 3 tp 1

---
+++ enseash-3.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#define WELCOME_MSG "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
+#define PROMPT "enseash%"
+#define EXIT "Bye bye..."
+int main() {
+    /* Buffer for storing the user's command */
+    char command[100];
+
+    /* Print the welcome message */
+    write(1, WELCOME_MSG, strlen(WELCOME_MSG));
+
+    /* Loop indefinitely to accept commands */
+    while (1) {
+        /* Print the shell prompt */
+        write(1, PROMPT, strlen(PROMPT));
+
+        /* Read the command from the user */
+        int bytes_read = read(0, command, 100);
+        command[bytes_read-1] = 0;
+
+        /* Check if the command is "exit" */
+        if (strcmp(command, "exit") == 0) {
+            /*Exit the loop and the program*/
+            write(1, EXIT, strlen(EXIT));
+            exit(0);
+
+        }
+        else {
+            /* Fork to create a child process*/
+            pid_t pid = fork();
+
+            /*Child process*/
+            if (pid == 0) {
+                execlp(command, command, (char *) NULL);
+            }
+                /*Parent process*/
+                /* Wait for the child process to finish*/
+            else {
+                wait(NULL);
+            }
+        }
+    }
+    return 0;
+}
